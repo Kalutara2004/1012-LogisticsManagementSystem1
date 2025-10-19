fac80933985500e3af726b2a2c3b4928d3fb63b4
Kalutara2004 <as20240596@sci.sjp.ac.lk>
2025-10-19 20:59:48 +0400

    Added system initialize function

--- Logistics_Management_System.c
+++ Logistics_Management_System.c
@@ -36,30 +36,77 @@ void distanceManagement(int cityCount,char cities[][MAX_NAME_LENGTH],int distanc
 void vehicleManagement(void);
 void deliveryRequest(int cityCount,char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES],int* deliveryCount,
                      int deliveryId[],char deliverySource[][MAX_NAME_LENGTH],
-                    char deliveryDestination[][MAX_NAME_LENGTH],float deliveryWeight[], float deliveryDistance[],int deliveryVehicle[],
+                    char deliveryDestination[][MAX_NAME_LENGTH],float deliveryWeight[],float deliveryDistance[],int deliveryVehicle[],
                     float deliveryCustomerCharge[],
-                    float deliveryEstimatedTime[], float deliveryPofit[]);
+                    float deliveryEstimatedTime[],float deliveryPofit[]);
 void reports(int deliveryCount,char deliverySource[][MAX_NAME_LENGTH],char deliveryDstination[][MAX_NAME_LENGTH],float deliveryDistance[],
-             float deliveryEstimatedTime[],float deliveryCustomerCharge[], float deliveryProfit[]);
-void fileHandling(int cityCount, char cities[][MAX_NAME_LENGTH], int distance[][MAX_CITIES]);
-void addCity(int* cityCount, char cities[][MAX_NAME_LENGTH], int distance[][MAX_CITIES]);
-void renameCity(int cityCount, char cities[][MAX_NAME_LENGTH],int deliveryCount,char deliverySource[][MAX_NAME_LENGTH],
+             float deliveryEstimatedTime[],float deliveryCustomerCharge[],float deliveryProfit[]);
+void fileHandling(int cityCount,char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES]);
+void addCity(int* cityCount,char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES]);
+void renameCity(int cityCount,char cities[][MAX_NAME_LENGTH],int deliveryCount,char deliverySource[][MAX_NAME_LENGTH],
                 char deliveryDestination[][MAX_NAME_LENGTH]);
-void removeCity(int* cityCount, char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES], int deliveryCount,
+void removeCity(int* cityCount,char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES],int deliveryCount,
                 char deliverySource[][MAX_NAME_LENGTH],char deliveryDestination[][MAX_NAME_LENGTH]);
-void displayCities(int cityCunt, char cities[][MAX_NAME_LENGTH]);
-int inputDistance(int cityCount, char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES]);
-void displayDistanceTable(int cityCount, char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES]);
-float calculateDeliveryCost(int source, int destination, int vehicleType,float weight, float minDistance,int vehicleRate[], int vehicleSpeed[],
-                           int vehicleEfficiency[], char cities[][MAX_NAME_LENGTH],char vehicle_types[][10]);
-void saveDeliveryRecord(int source, int destination, int vehicleType,
-                       float weight, float minDistance, int* deliveryCount,
-                       int deliveryId[], char deliverySource[][MAX_NAME_LENGTH],
+void displayCities(int cityCunt,char cities[][MAX_NAME_LENGTH]);
+int inputDistance(int cityCount,char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES]);
+void displayDistanceTable(int cityCount,char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES]);
+float calculateDeliveryCost(int source,int destination,int vehicleType,float weight,float minDistance,int vehicleRate[],int vehicleSpeed[],
+                           int vehicleEfficiency[],char cities[][MAX_NAME_LENGTH],char vehicle_types[][10]);
+void saveDeliveryRecord(int source,int destination,int vehicleType,
+                       float weight,float minDistance,int* deliveryCount,
+                       int deliveryId[],char deliverySource[][MAX_NAME_LENGTH],
                        char deliveryDestination[][MAX_NAME_LENGTH],
-                       float deliveryWeight[], float deliveryDistance[],
-                       int delivery_vehicle[], float deliveryCustomerCharge[],
-                       float deliveryEstimatedTime[], float deliveryProfit[],
-                       char cities[][MAX_NAME_LENGTH], int vehicleRate[],
+                       float deliveryWeight[],float deliveryDistance[],
+                       int delivery_vehicle[],float deliveryCustomerCharge[],
+                       float deliveryEstimatedTime[],float deliveryProfit[],
+                       char cities[][MAX_NAME_LENGTH],int vehicleRate[],
                        int vehicle_speed[]);
-float findMinimumDistance(int source, int destination, int path[], int* pathLength,int cityCount, int distance[][MAX_CITIES]);
+float findMinimumDistance(int source,int destination,int path[],int* pathLength,int cityCount,int distance[][MAX_CITIES]);
 void clearInputBuffer(void);
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+//Initialize the System
+void initializeSystem(int* cityCount, char cities[][MAX_NAME_LENGTH],int distance[][MAX_CITIES],int* deliveryCount) {
+    for(int i = 0; i < MAX_CITIES; i++) {
+        for(int j = 0; j < MAX_CITIES; j++) {
+            if (i == j) {
+                distance[i][j] = 0;
+            } else {
+                distance[i][j] = -1;
+            }
+        }
+    }
+
+char* cities[] = {"Colombo", "Kandy", "Galle", "Jaffna"};
+    int distancesA[4][4] = {
+        {0, 120, 115, 400},
+        {120, 0, 200, 350},
+        {115, 200, 0, 450},
+        {400, 350, 450, 0}
+    };
+
+    for(int i = 0; i < 4; i++) {
+        strcpy(cities[i],cities[i]);
+        for(int j = 0; j < 4; j++) {
+            distance[i][j] = distancesA[i][j];
+        }
+     }
+    *cityCount = 4;
+    *deliveryCount = 0;
+    printf("Logistics Management System Initialized!\n");
+}
